181caf7cf4b74bdd09150b7dac702c04b38ed0f6
kubasobon <wtty.fool@gmail.com>
2023-02-23 17:36:58 +0100

    nice

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kubasobon/semver-comparison
+
+go 1.19
+
+require (
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"testing"
+
+	masterminds "github.com/Masterminds/semver/v3"
+	blang "github.com/blang/semver/v4"
+	hashicorp "github.com/hashicorp/go-version"
+)
+
+type Case struct {
+	Constraint    string
+	Semvers       []string
+	ExpectedMatch bool
+}
+
+var testCases = []Case{}
+
+func TestHashicorp(t *testing.T) {
+	for _, testCase := range testCases {
+		constraint, err := hashicorp.NewConstraint(testCase.Constraint)
+		if err != nil {
+			t.Errorf("hashicorp lib could not parse %q constraint", testCase.Constraint)
+		}
+		for _, semverString := range testCase.Semvers {
+			semver, err := hashicorp.NewSemver(semverString)
+			if err != nil {
+				t.Errorf("hashicorp lib could not parse %q as semver", semverString)
+			}
+
+			outcome := constraint.Check(semver)
+			if outcome != testCase.ExpectedMatch {
+				t.Errorf(
+					"matching constraint %q with semver %q: expected %v, got %v",
+					testCase.Constraint, semverString, testCase.ExpectedMatch, outcome,
+				)
+			}
+		}
+	}
+}
+
+func TestBlang(t *testing.T) {
+	for _, testCase := range testCases {
+		constraint, err := blang.ParseRange(testCase.Constraint)
+		if err != nil {
+			t.Errorf("blang lib could not parse %q constraint", testCase.Constraint)
+		}
+		for _, semverString := range testCase.Semvers {
+			semver, err := blang.Make(semverString)
+			if err != nil {
+				t.Errorf("blang lib could not parse %q as semver", semverString)
+			}
+
+			outcome := constraint(semver)
+			if outcome != testCase.ExpectedMatch {
+				t.Errorf(
+					"matching constraint %q with semver %q: expected %v, got %v",
+					testCase.Constraint, semverString, testCase.ExpectedMatch, outcome,
+				)
+			}
+		}
+	}
+}
+
+func TestMasterminds(t *testing.T) {
+	for _, testCase := range testCases {
+		constraint, err := masterminds.NewConstraint(testCase.Constraint)
+		if err != nil {
+			t.Errorf("Masterminds lib could not parse %q constraint", testCase.Constraint)
+		}
+		for _, semverString := range testCase.Semvers {
+			semver, err := masterminds.NewVersion(semverString)
+			if err != nil {
+				t.Errorf("Masterminds lib could not parse %q as semver", semverString)
+			}
+
+			outcome := constraint.Check(semver)
+			if outcome != testCase.ExpectedMatch {
+				t.Errorf(
+					"matching constraint %q with semver %q: expected %v, got %v",
+					testCase.Constraint, semverString, testCase.ExpectedMatch, outcome,
+				)
+			}
+		}
+	}
+}
